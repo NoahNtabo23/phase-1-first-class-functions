9eb66e017486070a08145a18b1836df335e6f622
NoahNtabo23 <135032672+NoahNtabo23@users.noreply.github.com>
2023-06-15 21:45:46 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,22 @@
+function eatfood(){
+    console.log('Eat banana and bread.')
+}
+const receivesAFunction=(haveFun)=>{
+    eatfood();
+    haveFun('Go swimming.')
+
+}
+var goHome;
+goHome=()=>{
+
+}
+
+goHome=returnsANamedFunction=()=>{
+    return goHome
+}
+var sleepWell=()=>{}
+returnsAnAnonymousFunction(()=>{
+    return sleepWell
+
+})
+    
